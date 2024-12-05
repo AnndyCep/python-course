ce4a6ea99ea71bbfbbc528dd2979eb8ec6e903c8
Soy Andres <hannsonandrey@hotmail.com>
2024-12-04 19:27:09 -0500

    Sistaxis-caracteristicas

---
+++ python-basic/sintaxis-basica.py
@@ -0,0 +1,15 @@
+# Caracteristicas del lenguaje
+"""     Caracteristicas
+1. Lenguaje de proposito general (IA, JUEGOS, WEB, APLICACIONES, ANALISIS DE DATOS)
+2. Lenguaje de alto nivel, es decir que es mas facil de leerlo
+3. Facil de aprender, por su simulitud con el lenguaje humano, la comunidad es muy activa.
+4. Lenguaje tipado dimanico
+"""
+nombre = "Roberto" # Python se da cuenta qur tipo de dato es, se ajusta automaticamente
+valor = 3
+
+"""
+5. Lenguaje Orientado a objetos, clases, metodos, objetos
+6. Lenguaje interpretado, interpreta instruccion por intrucción, linea por linea.
+ 
+"""
