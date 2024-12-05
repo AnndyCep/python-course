fab27c7391225b071947ff05bf268cebfba994a7
Soy Andres <hannsonandrey@hotmail.com>
2024-12-04 19:53:58 -0500

    cambiando la jerarquia de nombres en los archivos .py agregando archivo con los datos simples en python en la rama feature/datos-simples

---
+++ python-basic/2-datos-simples.py
@@ -0,0 +1,12 @@
+# Datos simples en python
+"""Diferententes tipos de datos simples"""
+# 1. Tipo de datos Texto (String), cadena
+texto_una_linea = "Cadena de texto en una sola linea"
+texto_varias_lineas = """
+varias lineas
+de cadena
+"""
+# 2. Tipos de datos numeros : Enteros / Float
+entero = 4 , flotante = 4.0
+# 3. Tipo de dato boleano: True / False
+True , False
