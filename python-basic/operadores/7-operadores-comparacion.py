60b04868dd191025a1c82a306ec317fa8fd620cd
Soy Andres <hannsonandrey@hotmail.com>
2024-12-05 20:59:31 -0500

    Agregado carpetas operadores, tipos-datos , variable, como operadores de comparacion, describiendo cada uno de ellos

---
+++ python-basic/operadores/7-operadores-comparacion.py
@@ -0,0 +1,16 @@
+""" Operadores de comparación 
+== es igual
+!= es distinto
+< menor
+<= menor igual
+> mayor
+>= mayor igual
+
+"""
+# Ejemplos
+es_igual = 5 == 6 # False
+es_distinto = 5 != 6 # True
+mayor = 5 > 6 # False
+menor = 5 < 5 # True
+mayor_igual = 5 >= 6 # False
+menor_igual = 5 <= 6 # True
